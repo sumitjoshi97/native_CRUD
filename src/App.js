8d2cf92ca0107d0a65c11244bc658f361bac9244
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-13 15:00:55 +0530

    firebase initialization added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+// import libraries
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import firebase from 'firebase'
 
 // import components
-import Header from './components/common/Header'
+import Header from './components/common'
 
 class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDzk3lKxlY3sbsXkkh6mPjzh03DrkG926M',
+      authDomain: 'native-crud.firebaseapp.com',
+      databaseURL: 'https://native-crud.firebaseio.com',
+      projectId: 'native-crud',
+      storageBucket: 'native-crud.appspot.com',
+      messagingSenderId: '1053664661630'
+    })
+  }
+
   render() {
     return (
       <View>
-        <Header />
+        <Header headerText="Auth" />
         <Text>Name is SJ</Text>
       </View>
     )
